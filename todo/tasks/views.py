eab710262ee6c3fdce40cea787c1ba9ff1969a13
SeniorRolan <rolan0102@mail.ru>
2022-11-23 23:42:55 +0300

    added filter by select Model

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Truck
 from .forms import TruckForm
+import json
 from django.db.models import F
 
 
 def tasks_list(request):
     if request.method == 'POST':
+        # content = json.loads(request.body)
+        print(request.POST)
         form = TruckForm
-        tasks = Truck.objects.all().annotate(overload=(F('current_cargo') - F('truck_model__load_capacity')) * 100 / F('truck_model__load_capacity'))
+        tasks = Truck.objects.filter(truck_model__name=request.POST['truck_model']).annotate(overload=(F('current_cargo') - F('truck_model__load_capacity')) * 100 / F('truck_model__load_capacity'))
         if form.is_valid:
             return render(request, 'tasks/tasks_list.html', {'form': form, 'tasks': tasks})
     else:
