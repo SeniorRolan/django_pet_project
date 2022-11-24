8cb48f479c399138022d56f7ffb6b98f3df7b9e0
SeniorRolan <rolan0102@mail.ru>
2022-11-24 03:40:01 +0300

    code review

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -3,6 +3,5 @@ from .models import TruckModel, Truck
 
 
 class TruckForm(forms.Form):
-
-    truck_model = forms.ModelChoiceField(queryset=TruckModel.objects.all().distinct().values_list('name', flat=True),
-                                         empty_label='Все', label='Модель')
+    queryset = TruckModel.objects.all().distinct().values_list('name', flat=True)
+    truck_model = forms.ModelChoiceField(queryset=queryset, empty_label='Все', label='Модель')
